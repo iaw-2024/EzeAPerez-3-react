103b17b23fa4c70ce0e0deed0cd983e686ae72ee
EzePerez <ezequielalejandroperez@hotmail.com>
2024-04-05 16:30:56 -0300

    Se modifica index.html y se agregan los archivos a la carpeta src

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ListaJugadores from './ListaJugadores';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ListaJugadores />
+      </header>
+    </div>
+  );
+}
+
+export default App;
