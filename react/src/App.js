12cf5d8bf062579d8bb484533184abd9bbcd2d73
EzePerez <ezequielalejandroperez@hotmail.com>
2024-04-10 16:25:38 -0300

    Se crea la carpeta react  dentro de ella la carpeta public con sus respectivo index.html y la carpeta src con los componentes de react

---
+++ react/src/App.js
@@ -0,0 +1,15 @@
+import Nav from './Nav';
+import ListaJugadores from './ListaJugadores';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <ListaJugadores />
+      </header>
+    </div>
+  );
+}
+
+export default App;
